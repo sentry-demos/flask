18f06e7c47e14dbf9a4f2d537c57537bf7e438ed
William Capozzoli <thinkocapo@gmail.com>
2019-09-05 11:25:09 -0700

    fixed conflict. updated url in generate_events

--- generate_events.sh
+++ generate_events.sh
@@ -10,6 +10,6 @@ run_curl () {
 NAME=flask
 RANDOM_STRING=`openssl rand -base64 8`
 
-run_curl https://sentry-demos-$NAME.herokuapp.com/handled
-run_curl https://sentry-demos-$NAME.herokuapp.com/unhandled
-run_curl -X POST https://sentry-demos-$NAME.herokuapp.com/checkout -H "Content-Type: application/json" -H "X-Session-ID: _${RANDOM_STRING}" -H "X-Transaction-ID: _${RANDOM_STRING}" -d "{\"email\":\"$X@yahoo.com\", \"cart\":[{\"id\":\"wrench\", \"name\":\"Wrench\", \"price\":500}]}"
+run_curl https://will-flask-demo.herokuapp.com/handled
+run_curl https://will-flask-demo.herokuapp.com/unhandled
+run_curl -X POST https://will-flask-demo.herokuapp.com/checkout -H "Content-Type: application/json" -H "X-Session-ID: _${RANDOM_STRING}" -H "X-Transaction-ID: _${RANDOM_STRING}" -d "{\"email\":\"$X@yahoo.com\", \"cart\":[{\"id\":\"wrench\", \"name\":\"Wrench\", \"price\":500}]}"
